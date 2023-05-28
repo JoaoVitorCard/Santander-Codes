ae2942062789bc7a7e403b8f9f3d35661b885c76
João Vitor <joaovitonrnascim@gmail.com>
2023-05-28 17:09:29 -0300

    Add a new knowledge about java

--- primeiroDeMuitos/src/POOjava/Cachorro/Cachorro.java
+++ primeiroDeMuitos/src/POOjava/Cachorro/Cachorro.java
@@ -1,14 +1,55 @@
 package POOjava.Cachorro;
 
 public class Cachorro {
-  public String nome;
-  public String cor;
-  public int altura;
-  public double peso;
-  public int tamanhoDoRabo;
-  public String estadoDeEspirito;
+  private String nome;
+  private String cor;
+  private int altura;
+  private double peso;
+  private int tamanhoDoRabo;
+  private String estadoDeEspirito;
 
-  public void comer() {
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCor() {
+    return this.cor;
+  }
+
+  public void setCor(String cor) {
+    this.cor = cor;
+  }
+
+  public int getAltura() {
+    return this.altura;
+  }
+
+  public void setAltura(int altura) {
+    this.altura = altura;
+  }
+
+  public double getPeso() {
+    return this.peso;
+  }
+
+  public void setPeso(double peso) {
+    this.peso = peso;
+  }
+
+  public int getTamanhoDoRabo() {
+    return this.tamanhoDoRabo;
+  }
+
+  public void setTamanhoDoRabo(int tamanhoDoRabo) {
+    this.tamanhoDoRabo = tamanhoDoRabo;
+  }
+
+  public String getEstadoDeEspirito() {
+    return this.estadoDeEspirito;
   }
 
   public void latir() {
@@ -20,12 +61,30 @@ public class Cachorro {
   }
 
   public String interagir(String acao) {
-    if (acao.equals("Carinho")) {
-      this.estadoDeEspirito = "Feliz";
-    } else if (acao.equals("Cala a boca")) {
-      this.estadoDeEspirito = "Triste";
-    } else {
-      this.estadoDeEspirito = "Neutro";
+    // if (acao.equals("carinho")) {
+    // this.estadoDeEspirito = "Feliz";
+    // } else if (acao.equals("Cala a boca")) {
+    // this.estadoDeEspirito = "chateado";
+    // } else if (acao.equals("pisar na patinha")) {
+    // this.estadoDeEspirito = "triste";
+    // } else {
+    // this.estadoDeEspirito = "Neutro";
+    // }
+    // return estadoDeEspirito;
+
+    switch (acao) {
+      case "carinho":
+        this.estadoDeEspirito = "Feliz";
+        break;
+      case "cala a boca":
+        this.estadoDeEspirito = "Magoado";
+        break;
+      case "pisar na patinha":
+        this.estadoDeEspirito = "Triste";
+        break;
+      default:
+        this.estadoDeEspirito = "Neutro";
+        break;
     }
     return estadoDeEspirito;
   }
--- primeiroDeMuitos/src/POOjava/Encapsulameno.java
+++ primeiroDeMuitos/src/POOjava/Encapsulameno.java
@@ -1,5 +1,20 @@
 package POOjava;
 
-public class Encapsulameno {
+import POOjava.Cachorro.Cachorro;
 
+public class Encapsulameno {
+  public static void main(String[] args) {
+
+    Cachorro cachorro1 = new Cachorro();
+
+    cachorro1.setNome("Puppy");
+    cachorro1.setCor("Marrom");
+    cachorro1.setAltura(25);
+    cachorro1.setPeso(5.5);
+    cachorro1.setTamanhoDoRabo(5);
+
+    cachorro1.latir();
+    System.out.println("O cachorro pegou uma " + cachorro1.pegar());
+    System.out.println("O cachorro está " + cachorro1.interagir("cala a boca"));
+  }
 }
